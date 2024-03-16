5dda405b3f0c1d1de537fdc47b0fc2386f103b7c
codecraftmanX <chrismuneni72@gmail.com>
2024-03-16 14:42:12 +0300

    first commit

---
+++ personal_information.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Chris Muneni";
+  int age = 21;
+  String school = "KCA University";
+  String hobby = "playing football";
+
+  print("My name is $name, I am $age years old, I go to $school, and I enjoy $hobby.");
+}
